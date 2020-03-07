c37a8b68dc97a45fad5dfeb3533324e3ef8e8802
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-03-07 16:09:51 -0600

    Added Neomorphism Pattern

--- Meishi/Meishi/ContentView.swift
+++ Meishi/Meishi/ContentView.swift
@@ -10,7 +10,88 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Rectangle()
+                .fill(Color("Background"))
+                .frame(maxWidth: .infinity,  maxHeight: .infinity)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                HeaderCard()
+                .frame(width: 300, height: 180)
+                .modifier(Neomorphiser())
+            }
+            .padding(.top)
+            .animation(Animation.linear(duration: 1))
+        }
+    }
+}
+
+struct GithubCard : View {
+    var body: some View {
+        ZStack {
+            Rectangle()
+            .fill(Color("Background"))
+            VStack {
+                Text("@A01334390")
+                .fontWeight(.semibold)
+                .multilineTextAlignment(.center)
+                .padding()
+            }
+        }
+    }
+}
+
+struct HeaderCard : View {
+    var body: some View {
+            ZStack {
+                Rectangle()
+                .fill(Color("Background"))
+                VStack {
+                    Text("My name is Martin and I'm a mobile engineer")
+                        .fontWeight(.semibold)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                    HStack {
+                        Button(action: {
+                            print("wow")
+                        }, label: {
+                            Circle()
+                                .fill(Color("Background"))
+                                .frame(width: 50,height: 50)
+                        }).modifier(Neomorphiser())
+                        Button(action: {
+                            
+                        }, label: {
+                            Circle()
+                                .fill(Color("Background"))
+                                .frame(width: 50,height: 50)
+                        }).modifier(Neomorphiser())
+                        Button(action: {
+                            
+                        }, label: {
+                            Circle()
+                                .fill(Color("Background"))
+                                .frame(width: 50,height: 50)
+                        }).modifier(Neomorphiser())
+                        Button(action: {
+                            
+                        }, label: {
+                            Circle()
+                                .fill(Color("Background"))
+                                .frame(width: 50,height: 50)
+                        }).modifier(Neomorphiser())
+                    }
+                }
+            }
+        
+    }
+}
+
+struct Neomorphiser : ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .shadow(color: Color("LightShadow"), radius: 8, x: -8, y: -8)
+            .shadow(color: Color("DarkShadow"), radius: 8, x: 8, y: 8)
     }
 }
 
