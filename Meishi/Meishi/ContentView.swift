7a839d71f5455fcb7b0996f9cb03b651994d9498
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-03-07 15:33:50 -0600

    Added required files for Meishi Project

---
+++ Meishi/Meishi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Meishi
+//
+//  Created by Fernando Martin Garcia Del Angel on 07/03/20.
+//  Copyright © 2020 Fernando Martin Garcia Del Angel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
